8f42abeae736144a775468608c257ea56f46da4b
LUVBAY <luvbayelect@gmail.com>
2021-12-29 13:28:17 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>LUVBAY Webpage</title>
+    </head>
+    <body>
+        var nameNode = document.getElementById("name");
+        nameNode.addEventListener("click", function () {
+        alert("Welcome to LUVBAYWORLD!")
+        });
+    </body>
+</html>
